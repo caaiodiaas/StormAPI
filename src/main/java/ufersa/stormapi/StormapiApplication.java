98ca27499a8ac9a73e2570456cb12956daef1da9
Caio Dias <89219720+cdblader@users.noreply.github.com>
2022-06-20 23:14:42 -0300

    commit inicial

---
+++ src/main/java/ufersa/stormapi/StormapiApplication.java
@@ -0,0 +1,13 @@
+package ufersa.stormapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufersa/stormapi/StormapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ufersa.stormapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
